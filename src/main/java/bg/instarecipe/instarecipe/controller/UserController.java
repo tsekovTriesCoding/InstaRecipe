8bd60d79d101323d90ca28a48724656e9bcb072a
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-29 16:43:34 +0200

    add login and register views

---
+++ src/main/java/bg/instarecipe/instarecipe/controller/UserController.java
@@ -0,0 +1,19 @@
+package bg.instarecipe.instarecipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/login")
+    String login() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    String register() {
+        return "register";
+    }
+}
