e5f3c2f0da37658776dec6db8c7fe77ba2612cf2
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2024-12-29 17:29:40 +0200

    add view recipes

---
+++ src/main/java/bg/instarecipe/instarecipe/controller/RecipeController.java
@@ -0,0 +1,19 @@
+package bg.instarecipe.instarecipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/recipe")
+public class RecipeController {
+    @GetMapping
+    String recipe() {
+        return "recipes";
+    }
+
+    @GetMapping("/add")
+    String add() {
+        return "add-recipe";
+    }
+}
